6f10ab0cec96a000155a18f0a54d52f926add0eb
dhunter1991 <hunterdylan882@gmail.com>
2015-09-30 18:32:21 -0400

    variation 6

---
+++ asscoiatingjs1/js/hunter_dylan_output_.js
@@ -0,0 +1,52 @@
+alert("test");
+// Dylan Hunter
+// Class Scalable Data Infrastructures 01
+// output assignment
+
+// variables
+var piecesCut = 3000;
+var age = 24;
+var name = "Dylan Hunter";
+var lengthAtJob = "four years";
+var isEmployee = true;
+var nameOfEmployer = "Homestead Building Systems";
+var studyingMobileDevelopment = true;
+var plansToStudyMobileGaming = true;
+var phrase1 = 'I have been working in a lumber yard for four years now';
+var phrase2 = 'I am really excited to be studying mobile development here at Full Sail';
+var study = "Studying Mobile Development";
+var workLoad = "number of boards cut each day";
+var phrase3 = 'I am interested in creating my own mobile gaming compnay through my studies here';
+var plansToMakeMyOwnCompnay = true;
+
+
+
+
+
+
+
+// console log
+console.log(name);
+console.log(age);
+console.log(study);
+console.log(studyingMobileDevelopment);
+console.log(nameOfEmployer);
+console.log(lengthAtJob);
+console.log(isEmployee);
+console.log(workLoad);
+console.log(piecesCut);
+console.log(plansToStudyMobileGaming);
+console.log(phrase1);
+console.log(phrase2);
+console.log(phrase3);
+console.log(plansToMakeMyOwnCompnay);
+
+
+
+
+
+
+
+
+
+
