0f90a00b8a6fe3b4d0e38e6b7ac4f42335f31ebe
dhunter1991 <hunterdylan882@gmail.com>
2015-09-29 20:28:07 -0400

    first version

--- asscoiatingjs1/js/script.js
+++ asscoiatingjs1/js/script.js
@@ -1,5 +1,34 @@
 alert("test");
 
+// variables
+var piecesCut = 3000;
+var age = 24;
+var name = "Dylan Hunter";
+var lengthAtJob = "four years";
+var isEmployee = true;
+var nameOfEmployer = "homestead building systems";
+var studyingMobileDevelopment = true;
+var plansToStudyMobileGaming = true;
+var phrase1 = 'I have been working in a lumber yard for four years now';
+var phrase2 = 'I am really excited to be studying mobile development here at Full Sail';
+var phrase3 = 'I included information about my current job as well as some information about myself';
+
+
+
+// console log
+console.log(name);
+console.log(age);
+console.log(studyingMobileDevelopment);
+console.log(nameOfEmployer);
+console.log(lengthAtJob);
+console.log(isEmployee);
+console.log(piecesCut);
+console.log(plansToStudyMobileGaming);
+console.log(phrase1);
+console.log(phrase2);
+console.log(phrase3);
+
+
 
 
 
